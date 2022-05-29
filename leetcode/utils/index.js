89da12f039be8d252dedde622bf6c35b60890cdb
jintae.kimchi <jintae.kimchi@gmail.com>
2022-05-29 16:18:49 +0900

    [etc] javascript testing util

--- leetcode/utils/index.js
+++ leetcode/utils/index.js
@@ -1,3 +1,5 @@
+const assert = require("assert");
+
 exports.ListNode = function (val, next) {
   this.val = val === undefined ? 0 : val;
   this.next = next === undefined ? null : next;
@@ -16,4 +18,17 @@ exports.linkConcat = (node) => {
 exports.arrayResult = (array) => {
   if (!Array.isArray(array)) return "";
   return JSON.stringify(array);
+};
+
+exports.runTestCase = (tcList, solution, assertFn) => {
+  tcList.forEach(({ params, expect }) => {
+    if (!Array.isArray(params)) {
+      throw "테스트 파라미터는 배열 형태로 만들어야 함";
+    }
+    const result = solution(...params);
+    assertFn && typeof assertFn === "fuction"
+      ? assertFn(result, expect)
+      : assert.deepEqual(result, expect);
+  });
+  console.log("test done");
 };
