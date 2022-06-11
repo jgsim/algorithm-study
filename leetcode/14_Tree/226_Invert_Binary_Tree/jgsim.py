f95f9f0e62cc221546ccd3c52e0c92987d4b4069
jgsim <jgsim.official@gmail.com>
2022-06-11 22:34:26 +0900

    [leetcode] 226

---
+++ leetcode/14_Tree/226_Invert_Binary_Tree/jgsim.py
@@ -0,0 +1,27 @@
+# 226. Invert Binary Tree
+# Easy
+# https://leetcode.com/problems/invert-binary-tree/
+
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(node: TreeNode):
+            if not node:
+                return
+
+            node.left, node.right = node.right, node.left
+            
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+
+        return root
