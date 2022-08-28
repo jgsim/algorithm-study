9eacf96dac3fdad50930fecac22b0382016135d9
jgsim <jgsim.official@gmail.com>
2022-08-28 18:05:07 +0900

    모의테스트 답 수정

--- 모의테스트/jgsim/01.py
+++ 모의테스트/jgsim/01.py
@@ -1,18 +1,21 @@
 
+# 1: 예외 케이스 추가: zfill
+
 class Solution:
     def secretMap(self, n, arr1, arr2):
         result = []
         for i in range(n):
             v = arr1[i] | arr2[i]
             binStr = bin(v)[2:]
-            print(binStr)
-            print(binStr.replace('0', ' ').replace('1', '#'))
-            result.append(binStr.replace('0', ' ').replace('1', '#'))
+            result.append(
+                binStr.zfill(n)
+                .replace('0', ' ')
+                .replace('1', '#')
+            )
         return result
-            
 
 
 if __name__ == '__main__':
     s = Solution()
     assert s.secretMap(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]) == ['#####', '# # #', '### #', '#  ##', '#####']
-    print(s.secretMap(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]))
+    assert s.secretMap(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]) == ['######', '###  #', '##  ##', ' #### ', ' #####', '### # ']
--- 모의테스트/jgsim/05.py
+++ 모의테스트/jgsim/05.py
@@ -1,21 +1,20 @@
 from collections import Counter, defaultdict
 
+# 1: 집합원소 만들때, s를 미리 알파벳만 필터링 하여 오답 -> 집합원소를 만들고 필터링으로 변경
+# 2: union이 0일때 예외 처리 안함 -> 추가
+
 class Solution:
     def get_counter_dict(self, s: str):
-        cleared = [c for c in s.lower() if c.isalpha()]
-
         targets = []
-
-        for i in range(len(cleared) - 1):
-            targets.append(cleared[i] + cleared[i+1])
-
+        for i in range(len(s) - 1):
+            part = s[i] + s[i+1]
+            if part.isalpha():
+                targets.append(part.lower())
         counter = Counter(targets)
-
         return defaultdict(int, counter)
     
     def newsClustering(self, str1: str, str2: str) -> int:
         dict1, dict2 = self.get_counter_dict(str1), self.get_counter_dict(str2)
-
         union = inters = 0
         for target in dict1:
             union += max(dict1[target], dict2[target])
@@ -23,14 +22,13 @@ class Solution:
         for target in dict2:
             if dict1[target] == 0:
                 union += dict2[target]
-        return int(inters / union * 65536)
-
-
-
-
-        
+        jaccard_val = 1 if union == 0 else inters / union
+        return int(jaccard_val * 65536)
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.newsClustering('FRANCE', 'french'))
+    assert s.newsClustering('FRANCE', 'french') == 16384
+    assert s.newsClustering('handshake', 'shake hands') == 65536
+    assert s.newsClustering('aa1+aa2', 'AAAA12') == 43690
+    assert s.newsClustering('E=M*C^2', 'e=m*c^2') == 65536
