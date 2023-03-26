18d1be014e8c989dedd1619b78554f5e3d1a47d8
jgsim <jgsim.official@gmail.com>
2023-03-26 16:22:54 +0900

    Delete 2588 problem

--- leetcode-contest/336/2588_count-the-number-of-beautiful-subarrays/jgsim.py
+++
@@ -1,8 +0,0 @@
-# 2588. Count the Number of Beautiful Subarrays
-# Medium
-# https://leetcode.com/contest/weekly-contest-336/problems/count-the-number-of-beautiful-subarrays/
-from typing import List
-
-class Solution:
-    def beautifulSubarrays(self, nums: List[int]) -> int:
-        pass
